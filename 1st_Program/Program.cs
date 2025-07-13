f7a4668bd008ee5efdbfab11036be336980fc324
NirajTharu <nirajc279@gmail.com>
2025-07-13 12:24:02 +0545

    various inconvienence were deleted and the changes were done

--- 1st_Program/Program.cs
+++ 1st_Program/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 using System;
 
 namespace HelloWorld
@@ -8,18 +7,17 @@ namespace HelloWorld
   {
     static void Main(string[] args)
     {
-      //C sharp variables Example
       string name = "Niraj";
       int age = 22;
       /* C sharp multiple line comment 
       with the concatenation example
       */
-      Console.WriteLine("Warm Welcome everyone my name is " + name + " and my age is " + age);
+      Console.WriteLine("Welcome everyone my name is " + name + " and my age is " + age);
       Console.WriteLine("Enter your name:");
       name = Console.ReadLine();
       Console.WriteLine("Enter your age:");
       age = Convert.ToInt32(Console.ReadLine());
-      Console.WriteLine("Your name is "+name+" and age is "+age);
+      Console.WriteLine("Your name is " + name + " and age is " + age);
     }
   }
 }
