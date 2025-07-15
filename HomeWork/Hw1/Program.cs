164502afdf6c0ea6e42c576ee797e46544f3c76a
NirajTharu <nirajc279@gmail.com>
2025-07-15 09:11:35 +0545

    The addition of of the programs like loops and conditional and Homework too

---
+++ HomeWork/Hw1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Homework_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Input;
+            Console.WriteLine("Enter what do you want even or odd");
+            Input = Console.ReadLine();
+            if (Input == "even")
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+            else if(Input == "odd")
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The input is invalid! please even or odd according to your need");
+            }
+        }
+    }
+}
---
+++ loops/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+
+using System;
+
+namespace loops
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+            //  for loops 
+            Console.WriteLine("The even number are;");
+            for (int i = 2; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            // while loop 
+            Console.WriteLine("The odd numbers from 1 to 100 are;");
+            {
+                int y =0;
+                while (y <= 100)
+                {
+                    if (y % 2 != 0)
+                    {
+                        Console.WriteLine(y);
+                    }
+                    y++;
+                }
+            }
+
+    }
+  }
+}
