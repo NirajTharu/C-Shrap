e99884605c9bc40a6aef46450a9f8ef8ef735ff7
NirajTharu <nirajc279@gmail.com>
2025-07-20 00:38:34 +0545

    Adding the matrix in put in file in the c# folder

---
+++ Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr1 = new int[3,3];
+            int i, j;
+            Console.WriteLine("Enter the elements of 3*3 matrix:");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write($"Elements[{i},{j}]:");
+                    arr1[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            //Display the matrix
+            Console.WriteLine("\nThe matrix is:\n");
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    Console.Write("{0}\t", arr1[i, j]);
+                }
+                Console.WriteLine();            }
+            Console.WriteLine("\n\n");
+        }
+    }
+}
